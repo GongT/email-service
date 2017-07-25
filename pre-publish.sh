345984d09084efd64df39fe9925e30934055d727
GongT <admin@gongt.me>
2017-07-25 18:45:46 +0800

    update versions

--- pre-publish.sh
+++ pre-publish.sh
@@ -5,6 +5,11 @@ set -e
 tsc -p src
 tsc -p src/package
 unlink dist/npm-package/package.json || true
+
+cd src/package/
+ncu -a -u
+cd ../..
+
 cp src/package/package.json dist/npm-package/package.json
 cd dist/npm-package
 npm publish --access=public
